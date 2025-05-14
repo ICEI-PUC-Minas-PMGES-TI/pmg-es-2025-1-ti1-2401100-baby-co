38dfd9f0ec580873cbd258287b55c493519094eb
Mateus Ribas <marcalmateus315@gmail.com>
2025-05-14 16:14:24 -0300

    Criaçao dark mode

--- codigo/Mateus/js/app.js
+++ codigo/Mateus/js/app.js
@@ -205,3 +205,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 });
+
+
+
+  const toggle = document.getElementById('darkModeToggle');
+
+  toggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+  });   
