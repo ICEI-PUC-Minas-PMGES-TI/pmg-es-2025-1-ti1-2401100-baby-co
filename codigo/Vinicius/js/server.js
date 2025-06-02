aba77b047a4ba8f0f52815af8b2a4151a089543a
Vinícius Mantini <viniciusfmantinii@gmail.com>
2025-06-02 16:32:10 -0300

    artigos - .json server

---
+++ codigo/Vinicius/js/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(bodyParser.json());
+const postsFilePath = path.join(__dirname, '../json/posts.json');
+app.post('/salvar-post', (req, res) => {
+  const novoPost = req.body;
+  let posts = [];
+  if (fs.existsSync(postsFilePath)) {
+    posts = JSON.parse(fs.readFileSync(postsFilePath, 'utf8'));
+  }
+  posts.push(novoPost);
+  fs.writeFileSync(postsFilePath, JSON.stringify(posts, null, 2), 'utf8');
+  res.json({ mensagem: 'Post salvo com sucesso!' });
+});
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+app.get('/obter-posts', (req, res) => {
+  const postsFilePath = path.join(__dirname, '../json/posts.json');
+
+  if (fs.existsSync(postsFilePath)) {
+    const posts = JSON.parse(fs.readFileSync(postsFilePath, 'utf8'));
+    res.json(posts);
+  } else {
+    res.json([]);
+  }
+});
