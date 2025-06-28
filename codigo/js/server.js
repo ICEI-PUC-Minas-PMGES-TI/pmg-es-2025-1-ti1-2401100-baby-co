8ff15c7e4d2f057ed405d1fe3f0f325ff168f8d2
ruan alberto <ruanalbertosilva@gmail.com>
2025-06-28 10:31:27 -0300

    servindo arquivos estaticos com express

--- codigo/js/server.js
+++ codigo/js/server.js
@@ -3,12 +3,19 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const jsonServer = require('json-server');
+
 const app = express();
-app.use(express.static(path.join(__dirname, '../html')));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+// configs de middlewares do express
 app.use(cors());
 app.use(bodyParser.json());
+
+// path do db
 const postsFilePath = path.join(__dirname, '../db/db.json');
+
+// rotas 
 app.post('/salvar-post', (req, res) => {
   const novoPost = req.body;
   let posts = [];
@@ -19,16 +26,26 @@ app.post('/salvar-post', (req, res) => {
   fs.writeFileSync(postsFilePath, JSON.stringify(posts, null, 2), 'utf8');
   res.json({ mensagem: 'Post salvo com sucesso!' });
 });
+
+// usando node express para servir os arquivos estáticos do projeto
+app.use('/css', express.static(path.join(__dirname, '../css')));
+app.use('/js', express.static(path.join(__dirname))); // scripts JS
+app.use('/html', express.static(path.join(__dirname, '../html')));
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+
+// homepage rota
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../html/index.html'));
+});
+
+// rota da api usando json server
+const router = jsonServer.router(path.join(__dirname, '../db/db.json'));
+const middlewares = jsonServer.defaults();
+app.use('/', middlewares, router);
+
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
-app.get('/obter-posts', (req, res) => {
-  const postsFilePath = path.join(__dirname, '../db/db.json');
-
-  if (fs.existsSync(postsFilePath)) {
-    const posts = JSON.parse(fs.readFileSync(postsFilePath, 'utf8'));
-    res.json(posts);
-  } else {
-    res.json([]);
-  }
 });
